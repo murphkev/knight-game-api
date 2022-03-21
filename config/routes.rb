e5caa3dddd6c40f80b1f73f87e727ae213bdd295
Kevin Murphy <kevscottmurphy@gmail.com>
2022-03-21 12:53:38 +1100

    fixing route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources: :knight, only [:get_move]
+  resources :knight, only [:get_move]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
