b425eec5c53c5414b5d3a51917caa4af0ea93cb8
Kevin Murphy <kevscottmurphy@gmail.com>
2022-03-22 16:15:10 +1100

    adding model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    rescue_from ArgumentError, StandardError, :with => :handle_exception
+
+    def handle_exception(e)
+        render json: { error: e.message }, status: 400
 end
--- app/controllers/knight_controller.rb
+++ app/controllers/knight_controller.rb
@@ -1,18 +1,12 @@
 class KnightController < ApplicationController
   def get_move
     # try to convert query params to integers
-    begin
-      x = Integer(params[:x])
-      y = Integer(params[:y])
-      tx = Integer(params[:tx])
-      ty = Integer(params[:ty])
-      @path = Knight.get_path_to_target(x, y, tx, ty)
-      render json: { path: @path }, status: 200
-
-    # handle non-numeric inputs & impossible case of no move found
-    rescue ArgumentError, StandardError => e
-      render json: { error: e.message }, status: 400
-
+    x = Integer(params[:x])
+    y = Integer(params[:y])
+    tx = Integer(params[:tx])
+    ty = Integer(params[:ty])
+    @path = Knight.get_path_to_target(x, y, tx, ty)
+    render json: { path: @path }, status: 200
     end
   end
 end
