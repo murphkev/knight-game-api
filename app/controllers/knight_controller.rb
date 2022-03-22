1daf5e4d0b2b47976a47f48db1dcdb85dbcf0cb3
Kevin Murphy <kevscottmurphy@gmail.com>
2022-03-22 16:40:31 +1100

    removing parentheses

--- app/controllers/knight_controller.rb
+++ app/controllers/knight_controller.rb
@@ -1,10 +1,10 @@
 class KnightController < ApplicationController
-  def get_move
+  def get_path
     # try to convert query params to integers
-    x = Integer(params[:x])
-    y = Integer(params[:y])
-    tx = Integer(params[:tx])
-    ty = Integer(params[:ty])
+    x = Integer params[:x]
+    y = Integer params[:y]
+    tx = Integer params[:tx]
+    ty = Integer params[:ty]
     @path = Knight.get_path_to_target(x, y, tx, ty)
     render json: { path: @path }, status: 200
   end
--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,12 +1,10 @@
 class Knight
-
-def self.get_path_to_target(x, y, tx, ty)
-
+  def self.get_path_to_target(x, y, tx, ty) 
     pos = [x, y]
     target = [tx, ty]
 
     # check pos and target
-    if !is_valid_square(pos) || !is_valid_square(target) || is_same_square(pos, target) then
+    if !is_valid_square pos || !is_valid_square target || is_same_square(pos, target) then
       raise ArgumentError.new "Invalid position or target (#{pos[0]},#{pos[1]}) to (#{target[0]},#{target[1]})"
     end
 
@@ -15,14 +13,14 @@ def self.get_path_to_target(x, y, tx, ty)
     delta_y = [-1, -2, -2, -1,  1,  2, 2, 1]
 
     # queue array containing array { pos, history }
-    queue = Array.new(1) {Array.new(2)}
+    queue = Array.new 1 {Array.new 2}
     queue[0][0] = pos
     queue[0][1] = []
     
     # boolean version of chess board to check visited squares
     # so that we don't revisit the same square and end up in
     # an infinite loop
-    visited = Array.new(8) {Array.new(8, false)}
+    visited = Array.new 8 {Array.new(8, false)}
 
     # mark the current knight pos as visited
     visited[pos[0]][pos[1]] = true;
@@ -31,7 +29,7 @@ def self.get_path_to_target(x, y, tx, ty)
     new_pos, x, y = 0, 0, 0
 
     # loop until there is one element in queue
-    while queue.length() != 0 do
+    while queue.length != 0 do
         # get the first pos
         
         # NB: because positions are added in order of 
@@ -40,7 +38,7 @@ def self.get_path_to_target(x, y, tx, ty)
         # distance of moves; the first element will always
         # have the lowest, or equal lowest move count.
         
-        new_pos = queue.shift();
+        new_pos = queue.shift;
 
         # if that's the target...
         if is_same_square(new_pos[0], target) then 
@@ -57,12 +55,12 @@ def self.get_path_to_target(x, y, tx, ty)
             xy = [x, y]
 
             # make sure those squares are valid, and not previously visited
-            if is_valid_square(xy) && !visited[x][y] then
+            if is_valid_square xy && !visited[x][y] then
                 # mark square as visited
                 visited[x][y] = true;
                 history = new_pos[1].deep_dup
-                history.push(xy)
-                queue.push([xy, history])
+                history.push xy
+                queue.push [xy, history]
             end
         end
     end
@@ -73,8 +71,8 @@ def self.get_path_to_target(x, y, tx, ty)
 
   # checks to see if two positions are the same
   def self.is_same_square(pos1, pos2) 
-      if pos1.length() == 2 && pos2.length() == 2 then
-          return pos1[0].eql?(pos2[0]) && pos1[1].eql?(pos2[1])
+      if pos1.length == 2 && pos2.length == 2 then
+          return pos1[0].eql? pos2[0] && pos1[1].eql? pos2[1]
       else
           return false
       end
@@ -82,7 +80,7 @@ def self.get_path_to_target(x, y, tx, ty)
 
   # checks to see if a square is valid for the board
   def self.is_valid_square(pos) 
-      if pos.length() == 2 then
+      if pos.length == 2 then
           return pos[0].between?(0, 7) && pos[1].between?(0, 7)
       else
           return false
